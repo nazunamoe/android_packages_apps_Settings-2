290bbb45c79b4ead1034a717da98a4929bcc965d
Daniel MM <daxx4182@gmail.com>
2012-07-29 09:20:28 -0400

    Port Forward: Show full Kernel proc/version information
    Ported from AOKP : https://github.com/AOKP/packages_apps_Settings/commit/eddaad59074fadbb7d6a59958ef182d66d218302
    
    Change-Id: I2b211fb72c25cc119f2a464b04821fcef77a8908

--- src/com/android/settings/DeviceInfoSettings.java
+++ src/com/android/settings/DeviceInfoSettings.java
@@ -85,7 +85,8 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment {
         setStringSummary(KEY_DEVICE_MODEL, Build.MODEL);
         setStringSummary(KEY_BUILD_NUMBER, Build.DISPLAY);
         findPreference(KEY_BUILD_NUMBER).setEnabled(true);
-        findPreference(KEY_KERNEL_VERSION).setSummary(getFormattedKernelVersion());
+        setStringSummary(KEY_KERNEL_VERSION, getFormattedKernelVersion());
+        findPreference(KEY_KERNEL_VERSION).setEnabled(true);
         setValueSummary(KEY_MOD_VERSION, "ro.xylon.version");
 
         if (!SELinux.isSELinuxEnabled()) {
@@ -162,7 +163,8 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment {
 
     @Override
     public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
-        if (preference.getKey().equals(KEY_FIRMWARE_VERSION)) {
+        String prefKey = preference.getKey();
+        if (prefKey.equals(KEY_FIRMWARE_VERSION)) {
             System.arraycopy(mHits, 1, mHits, 0, mHits.length-1);
             mHits[mHits.length-1] = SystemClock.uptimeMillis();
             if (mHits[0] >= (SystemClock.uptimeMillis()-500)) {
@@ -175,6 +177,9 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment {
                     Log.e(LOG_TAG, "Unable to start activity " + intent.toString());
                 }
             }
+        } else if (prefKey.equals(KEY_KERNEL_VERSION)) {
+            setStringSummary(KEY_KERNEL_VERSION, getKernelVersion());
+            return true;
         } else if (preference.getKey().equals(KEY_BUILD_NUMBER)) {
             if (mDevHitCountdown > 0) {
                 mDevHitCountdown--;
@@ -266,7 +271,23 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment {
         }
     }
 
-    public static String getFormattedKernelVersion() {
+    private String getKernelVersion() {
+        String procVersionStr;
+        try {
+            procVersionStr = readLine(FILENAME_PROC_VERSION);
+            return procVersionStr;
+        } catch (IOException e) {
+            Log.e(LOG_TAG,
+                "IO Exception when getting kernel version for Device Info screen",
+                e);
+
+            return "Unavailable";
+        }
+    }
+
+    private String getFormattedKernelVersion() {
+        String procVersionStr;
+
         try {
             return formatKernelVersion(readLine(FILENAME_PROC_VERSION));
 
