4dfbfe7401720cc63b61e92088cd9e4c7fe9d273
Daniel <daxx4182@gmail.com>
2012-12-24 00:13:40 +0800

    Cleanup time (1/3)
    XY features settings will be now moved to it's own Settings. So it's time for the heavy cleanup.
    
    Change-Id: I10708e85b61a9648416e13248f6a83fc2ae65ffd

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -120,7 +120,9 @@ public class Settings extends PreferenceActivity
             R.id.system_section,
             R.id.date_time_settings,
             R.id.about_settings,
-            R.id.accessibility_settings
+            R.id.accessibility_settings,
+            R.id.xylon_section,
+            R.id.xy_settings,
     };
 
     private SharedPreferences mDevelopmentPreferences;
