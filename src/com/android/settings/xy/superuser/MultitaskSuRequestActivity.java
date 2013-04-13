Root-Box/packages_apps_Settings

Embed Superuser into System Settings

src/com/android/settings/rb/superuser/MultitaskSuRequestActivity.java
@@ -1,0 +1,4 @@
+package com.android.settings.rb.superuser;
+
+public class MultitaskSuRequestActivity extends com.koushikdutta.superuser.MultitaskSuRequestActivity {
+}

src/com/android/settings/rb/superuser/NotifyActivity.java
@@ -1,0 +1,4 @@
+package com.android.settings.rb.superuser;
+
+public class NotifyActivity extends com.koushikdutta.superuser.NotifyActivity {
+}

src/com/android/settings/rb/superuser/PolicyNativeFragment.java
@@ -1,0 +1,4 @@
+package com.android.settings.rb.superuser;
+
+public class PolicyNativeFragment extends com.koushikdutta.superuser.PolicyNativeFragment {
+}

src/com/android/settings/rb/superuser/RequestActivity.java
@@ -1,0 +1,4 @@
+package com.android.settings.rb.superuser;
+
+public class RequestActivity extends com.koushikdutta.superuser.RequestActivity {
+}

src/com/android/settings/rb/superuser/SuReceiver.java
@@ -1,0 +1,4 @@
+package com.android.settings.rb.superuser;
+
+public class SuReceiver extends com.koushikdutta.superuser.SuReceiver {
+}

nazunamoe/android_packages_apps_Settings-2

src/com/android/settings/xy/superuser/MultitaskSuRequestActivity.java
@@ -1,0 +1,4 @@
+package com.android.settings.xy.superuser;
+
+public class MultitaskSuRequestActivity extends com.koushikdutta.superuser.MultitaskSuRequestActivity {
+}

src/com/android/settings/xy/superuser/NotifyActivity.java
@@ -1,0 +1,4 @@
+package com.android.settings.xy.superuser;
+
+public class NotifyActivity extends com.koushikdutta.superuser.NotifyActivity {
+}

src/com/android/settings/xy/superuser/PolicyNativeFragment.java
@@ -1,0 +1,4 @@
+package com.android.settings.xy.superuser;
+
+public class PolicyNativeFragment extends com.koushikdutta.superuser.PolicyNativeFragment {
+}

src/com/android/settings/xy/superuser/RequestActivity.java
@@ -1,0 +1,4 @@
+package com.android.settings.xy.superuser;
+
+public class RequestActivity extends com.koushikdutta.superuser.RequestActivity {
+}

src/com/android/settings/xy/superuser/SuReceiver.java
@@ -1,0 +1,4 @@
+package com.android.settings.xy.superuser;
+
+public class SuReceiver extends com.koushikdutta.superuser.SuReceiver {
+}