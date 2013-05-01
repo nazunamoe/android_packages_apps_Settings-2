2aa08321304f68d61b123804d30eac25682fe5a5
Kyler Jeffrey <mbqsniper@hotmail.com>
2013-05-01 13:16:10 -0700

    Hardcode my CPU Guide into Settings (Testing this out, will revert this if it causes build errors or user issues)

---
+++ src/com/android/settings/mbqscpuguide/R$id.java
@@ -0,0 +1,152 @@
+.class public final Lcom/MBQmbqscpuguide/R$id;
+.super Ljava/lang/Object;
+.source "R.java"
+
+
+# annotations
+.annotation system Ldalvik/annotation/EnclosingClass;
+    value = Lcom/MBQmbqscpuguide/R;
+.end annotation
+
+.annotation system Ldalvik/annotation/InnerClass;
+    accessFlags = 0x19
+    name = "id"
+.end annotation
+
+
+# static fields
+.field public static final Guide:I = 0x7f080000
+
+.field public static final action_settings:I = 0x7f08003d
+
+.field public static final tab1:I = 0x7f080001
+
+.field public static final tab2:I = 0x7f08003b
+
+.field public static final tab3:I = 0x7f08003c
+
+.field public static final textView1:I = 0x7f080002
+
+.field public static final textView10:I = 0x7f08000b
+
+.field public static final textView11:I = 0x7f08000c
+
+.field public static final textView12:I = 0x7f08000d
+
+.field public static final textView13:I = 0x7f08000e
+
+.field public static final textView14:I = 0x7f08000f
+
+.field public static final textView15:I = 0x7f080010
+
+.field public static final textView16:I = 0x7f080011
+
+.field public static final textView17:I = 0x7f080012
+
+.field public static final textView18:I = 0x7f080013
+
+.field public static final textView19:I = 0x7f080014
+
+.field public static final textView2:I = 0x7f080003
+
+.field public static final textView20:I = 0x7f080015
+
+.field public static final textView21:I = 0x7f080016
+
+.field public static final textView22:I = 0x7f080017
+
+.field public static final textView23:I = 0x7f080018
+
+.field public static final textView24:I = 0x7f080019
+
+.field public static final textView25:I = 0x7f08001a
+
+.field public static final textView26:I = 0x7f08001b
+
+.field public static final textView27:I = 0x7f08001c
+
+.field public static final textView28:I = 0x7f08001d
+
+.field public static final textView29:I = 0x7f08001e
+
+.field public static final textView3:I = 0x7f080004
+
+.field public static final textView30:I = 0x7f08001f
+
+.field public static final textView31:I = 0x7f080020
+
+.field public static final textView32:I = 0x7f080021
+
+.field public static final textView33:I = 0x7f080022
+
+.field public static final textView34:I = 0x7f080023
+
+.field public static final textView35:I = 0x7f080024
+
+.field public static final textView36:I = 0x7f080025
+
+.field public static final textView37:I = 0x7f080026
+
+.field public static final textView38:I = 0x7f080027
+
+.field public static final textView39:I = 0x7f080028
+
+.field public static final textView4:I = 0x7f080005
+
+.field public static final textView40:I = 0x7f080029
+
+.field public static final textView41:I = 0x7f08002a
+
+.field public static final textView42:I = 0x7f08002b
+
+.field public static final textView43:I = 0x7f08002c
+
+.field public static final textView44:I = 0x7f08002d
+
+.field public static final textView45:I = 0x7f08002e
+
+.field public static final textView46:I = 0x7f08002f
+
+.field public static final textView47:I = 0x7f080030
+
+.field public static final textView48:I = 0x7f080031
+
+.field public static final textView49:I = 0x7f080032
+
+.field public static final textView5:I = 0x7f080006
+
+.field public static final textView50:I = 0x7f080033
+
+.field public static final textView51:I = 0x7f080034
+
+.field public static final textView52:I = 0x7f080035
+
+.field public static final textView53:I = 0x7f080036
+
+.field public static final textView54:I = 0x7f080037
+
+.field public static final textView55:I = 0x7f080038
+
+.field public static final textView56:I = 0x7f080039
+
+.field public static final textView57:I = 0x7f08003a
+
+.field public static final textView6:I = 0x7f080007
+
+.field public static final textView7:I = 0x7f080008
+
+.field public static final textView8:I = 0x7f080009
+
+.field public static final textView9:I = 0x7f08000a
+
+
+# direct methods
+.method public constructor <init>()V
+    .locals 0
+
+    .prologue
+    .line 26
+    invoke-direct {p0}, Ljava/lang/Object;-><init>()V
+
+    return-void
+.end method
---
+++ src/com/android/settings/mbqscpuguide/R$string.java
@@ -0,0 +1,146 @@
+.class public final Lcom/MBQmbqscpuguide/R$string;
+.super Ljava/lang/Object;
+.source "R.java"
+
+
+# annotations
+.annotation system Ldalvik/annotation/EnclosingClass;
+    value = Lcom/MBQmbqscpuguide/R;
+.end annotation
+
+.annotation system Ldalvik/annotation/InnerClass;
+    accessFlags = 0x19
+    name = "string"
+.end annotation
+
+
+# static fields
+.field public static final BrazilianWax:I = 0x7f05002f
+
+.field public static final BrazilianWaxInfo:I = 0x7f050030
+
+.field public static final Conservative:I = 0x7f05000f
+
+.field public static final ConservativeInfo:I = 0x7f050010
+
+.field public static final DynInteractive:I = 0x7f050039
+
+.field public static final DynInteractiveInfo:I = 0x7f05003a
+
+.field public static final HotPlug:I = 0x7f05001d
+
+.field public static final HotPlugInfo:I = 0x7f05001e
+
+.field public static final HotPlugX:I = 0x7f050025
+
+.field public static final HotPlugXInfo:I = 0x7f050026
+
+.field public static final IntelliDemand:I = 0x7f050015
+
+.field public static final IntelliDemandInfo:I = 0x7f050016
+
+.field public static final IntelliDemandX:I = 0x7f050017
+
+.field public static final IntelliDemandXInfo:I = 0x7f050018
+
+.field public static final Interactive:I = 0x7f050007
+
+.field public static final InteractiveInfo:I = 0x7f050008
+
+.field public static final InteractiveX:I = 0x7f050009
+
+.field public static final InteractiveXInfo:I = 0x7f05000a
+
+.field public static final LagFree:I = 0x7f050013
+
+.field public static final LagFreeInfo:I = 0x7f050014
+
+.field public static final Lazy:I = 0x7f050021
+
+.field public static final LazyInfo:I = 0x7f050022
+
+.field public static final LionHeart:I = 0x7f05002d
+
+.field public static final LionHeartInfo:I = 0x7f05002e
+
+.field public static final LulzActive:I = 0x7f050027
+
+.field public static final LulzActiveInfo:I = 0x7f050028
+
+.field public static final Min_Max:I = 0x7f050019
+
+.field public static final Min_Max_Info:I = 0x7f05001a
+
+.field public static final Nightmare:I = 0x7f050023
+
+.field public static final NightmareInfo:I = 0x7f050024
+
+.field public static final OnDemand:I = 0x7f050003
+
+.field public static final OnDemandPlus:I = 0x7f050037
+
+.field public static final OnDemandPlusInfo:I = 0x7f050038
+
+.field public static final OnDemandX:I = 0x7f050005
+
+.field public static final OnDemandXInfo:I = 0x7f050006
+
+.field public static final PegasusQ:I = 0x7f05001f
+
+.field public static final PegasusQInfo:I = 0x7f050020
+
+.field public static final Performance:I = 0x7f05000b
+
+.field public static final PerformanceInfo:I = 0x7f05000c
+
+.field public static final PowerSave:I = 0x7f05000d
+
+.field public static final PowerSaveInfo:I = 0x7f05000e
+
+.field public static final Sakuractive:I = 0x7f050035
+
+.field public static final SakuractiveInfo:I = 0x7f050036
+
+.field public static final SavagedZen:I = 0x7f050031
+
+.field public static final SavagedZenInfo:I = 0x7f050032
+
+.field public static final Scary:I = 0x7f050033
+
+.field public static final ScaryInfo:I = 0x7f050034
+
+.field public static final SmartAss:I = 0x7f050029
+
+.field public static final SmartAssInfo:I = 0x7f05002a
+
+.field public static final SmartAssV2:I = 0x7f05002b
+
+.field public static final SmartAssV2Info:I = 0x7f05002c
+
+.field public static final UserSpace:I = 0x7f050011
+
+.field public static final UserSpaceInfo:I = 0x7f050012
+
+.field public static final Wheatley:I = 0x7f05001b
+
+.field public static final WheatleyInfo:I = 0x7f05001c
+
+.field public static final action_settings:I = 0x7f050001
+
+.field public static final app_name:I = 0x7f050000
+
+.field public static final basically_this_governor_will_allow_your_phone_to_use_cpu_speeds_on_demand_meaning_if_you_re_sending_a_text_your_phone_wont_require_much_memory_but_if_you_re_playing_a_graphically_intense_game_it_will_use_both_cores_most_likely_at_your_highest_set_cpu_speed_and_will_idle_back_down_when_you_finish_your_game_:I = 0x7f050004
+
+.field public static final mbqs_cpu_guide:I = 0x7f050002
+
+
+# direct methods
+.method public constructor <init>()V
+    .locals 0
+
+    .prologue
+    .line 96
+    invoke-direct {p0}, Ljava/lang/Object;-><init>()V
+
+    return-void
+.end method
